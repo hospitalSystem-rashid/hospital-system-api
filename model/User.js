412588b8c09a76706bf6c3e78735070f8d5d4e5f
AbdulRashid <abdhulrashidh2002@gmail.com>
2026-01-24 20:19:01 +0530

    init

---
+++ model/User.js
@@ -0,0 +1,28 @@
+const mongoose=require("mongoose");
+
+const Userschema=
+    new mongoose.Schema({
+        name:{type:String, required:true},
+        email:{type:email, required:true, unique:true},
+        phone:{type:String},
+        role:{
+            type:String,
+            enum:["admin","doctor","nurse","lab","pharmacist","receptionist"],
+            required:true
+        },
+        password:{type:String, required:true},
+        profileImage:{type:String},
+
+        resetToken:{type:String},
+        resetTokenExpiry:{type:Date},
+
+        specialization:{type:String},
+        qualification:{type:String},
+        experience:{type:Number},
+        availability:[
+            {day:String, startTime:String,endTime:String}
+        ],
+
+    }, {timestamps:true});
+
+module.exports=mongoose.model('User', Userschema);
