b8e282a5b9981e9bec886e29871c578f7c95606c
AbdulRashid <abdhulrashidh2002@gmail.com>
2026-01-26 23:34:59 +0530

    Medicine and Prescription created

---
+++ model/Medicine.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const MedicineSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    batchNumber: {type: Number},
+    expiryDate: {type: Date},
+    stock: {type: Number, required: true},
+    price: {type: Number, required: true},
+    lowStockAlert: {type: Number, default: 10}
+
+
+}, {timestamps: true});
+
+module.exports = mongoose.model("Medicine", MedicineSchema);
