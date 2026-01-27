121360edb00cc2d90b678bd2a7b6e3c8978ee1fb
AbdulRashid <abdhulrashidh2002@gmail.com>
2026-01-27 21:25:23 +0530

    working on API

--- route/AdmissionRoute.js
+++ route/AdmissionRoute.js
@@ -5,14 +5,21 @@ const {
     createAdmission,
     getAdmissions,
     getAdmission,
+    getAdmissionsByPatient,
     updateAdmission,
+    addProgressNote,
     deleteAdmission
 } = require("../controller/AdmissionController");
 
+router.post("/", createAdmission);
 
 router.get("/", getAdmissions);
 router.get("/:id", getAdmission);
+router.get("/patient/:patientId", getAdmissionsByPatient);
+
 router.put("/:id", updateAdmission);
+router.put("/:id/progress", addProgressNote);
+
 router.delete("/:id", deleteAdmission);
 
 module.exports = router;
